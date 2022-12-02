1cc0ac2787e45a1835f3c93d02f8d333fe86fbfc
HuseyinCanSimsek <118460954+HuseyinCanSimsek@users.noreply.github.com>
2022-12-02 18:28:38 +0300

    Add files via upload

---
+++ ecommerce-pair1-spring.workshop/src/main/java/com/etiya/ecommercedemopair1/repository/abstracts/CategoryRepository.java
@@ -0,0 +1,14 @@
+package com.etiya.ecommercedemopair1.repository.abstracts;
+
+import com.etiya.ecommercedemopair1.entities.concretes.Category;
+import com.etiya.ecommercedemopair1.entities.concretes.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface CategoryRepository extends JpaRepository<Category,Integer> {
+    List<Category> findAllByName(String name);
+    @Query("select c from Category c order by c.id desc")
+    List<Category> getCategoryWithIdDesc();
+}
